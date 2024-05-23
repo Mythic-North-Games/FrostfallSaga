b9772dc58631e4f1fee07686b1876123c2646b01
Alexandre Sparton <alexandre.sparton@epitech.eu>
2024-05-23 23:36:25 +0200

    feat: template tests added for HexMetrics + file structure refactored

---
+++ Assets/Code/Tests/EditMode/HexGrid/HexMetricsTests.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+
+public class HexMetricsTests
+{
+    [Test]
+    public void OuterRadius_Test()
+    {
+        // Arrange
+        float hexagoneSize = 10f;
+        float expectedOuterRadius = 10f;
+
+        // Act
+        float outerRadius = HexMetrics.OuterRadius(hexagoneSize);
+
+        // Assert
+        Assert.AreEqual(expectedOuterRadius, outerRadius);
+    }
+
+    [Test]
+    public void InnerRadius_Test()
+    {
+        // Arrange
+        float hexagoneSize = 10f;
+        float expectedInnerRadius = 8.66025352f;
+
+        // Act
+        float innerRadius = HexMetrics.InnerRadius(hexagoneSize);
+
+        // Assert
+        Assert.AreEqual(expectedInnerRadius, innerRadius);
+    }
+
+    [Test]
+    public void OuterRadius_GreaterThen_InnerRadius_Test()
+    {
+        // Arrange
+        float hexagoneSize = 10f;
+
+        // Act
+        float outerRadius = HexMetrics.OuterRadius(hexagoneSize);
+        float innerRadius = HexMetrics.InnerRadius(hexagoneSize);
+
+        // Assert
+        Assert.Greater(outerRadius, innerRadius);
+    }
+}
