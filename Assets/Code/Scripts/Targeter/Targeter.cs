5b8ff510e8275f66200b6f757342d6de4eba9284
Alexis <alexis.guyon@epitech.eu>
2024-05-28 15:25:51 +0200

    [FEAT] Adding Targeter

---
+++ Assets/Code/Scripts/Targeter/Targeter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Code.Scripts.Targeter
+{
+    public class Targeter
+    {
+        [SerializeField] private int _originCellRange;
+        [SerializeField] private Vector3[] _cellsSequence;
+        [SerializeField] private bool _fighterMandatory;
+
+        public int OriginCellRange
+        {
+            get => _originCellRange;
+        }
+
+        public Vector3[] CellsSequence
+        {
+            get => _cellsSequence;
+        }
+
+        public bool FighterMandatory
+        {
+            get => _fighterMandatory;
+        }
+    }
+}
