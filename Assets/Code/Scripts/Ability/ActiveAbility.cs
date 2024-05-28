452f105fa48cd1421fb4e5c426fecafd3528b368
Alexis <alexis.guyon@epitech.eu>
2024-05-28 15:26:41 +0200

    [FEAT] Adding ActiveAbility

---
+++ Assets/Code/Scripts/Ability/ActiveAbility.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Code.Scripts.Targeter;
+
+namespace Code.Scripts.Ability
+{
+    public class ActiveAbility
+    {
+        
+        [SerializeField] private Targeter.Targeter _targeter;
+        [SerializeField] private int _actionsPointsCost;
+
+        public Targeter.Targeter Targeter
+        {
+            get => _targeter;
+        }
+
+        public int ActionsPointsCost
+        {
+            get => _actionsPointsCost;
+        }
+    }
+}
