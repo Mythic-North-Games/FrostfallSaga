bf15ba47b9695e9dd9b1c6801398d4655aa2d573
Alexandre Sparton <alexandre.sparton@epitech.eu>
2024-06-03 18:00:50 +0200

    feat: ability SO + ability prefab added + mouse controller updated

---
+++ Assets/Code/Scripts/Fight/Ability/BaseAbility.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+namespace FrostfallSaga.Fight.Abilities
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class BaseAbility : MonoBehaviour
+    {
+        [field: SerializeField] public BaseAbilityScriptableObject AbilityData { get; private set; }
+
+        private void Reset()
+        {
+            if (AbilityData == null)
+            {
+                Debug.LogWarning("No AbilityData set for ability " + name);
+                return;
+
+            }
+
+            if (AbilityData.IconSprite == null)
+            {
+                Debug.LogWarning("No IconSprite set for ability " + name);
+                return;
+            }
+
+            GetComponent<SpriteRenderer>().sprite = AbilityData.IconSprite;
+        }
+
+        private void OnEnable()
+        {
+            if (AbilityData == null)
+            {
+                Debug.LogError("No AbilityData set for ability " + name);
+                return;
+
+            }
+
+            if (AbilityData.IconSprite == null)
+            {
+                Debug.LogError("No IconSprite set for ability " + name);
+                return;
+            }
+
+            GetComponent<SpriteRenderer>().sprite = AbilityData.IconSprite;
+        }
+    }
+}
---
+++ Assets/Code/Scripts/Fight/Ability/BaseAbilityScriptableObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using FrostfallSaga.Fight.Effects;
+
+
+namespace FrostfallSaga.Fight.Abilities
+{
+    /// <summary>
+    /// Abilities common fields.
+    /// </summary>
+    public abstract class BaseAbilityScriptableObject : ScriptableObject
+    {
+        [field: SerializeField] public string Name { get; private set; }
+        [field: SerializeField] public string Description { get; private set; }
+        [field: SerializeField] public Sprite IconSprite { get; private set; }
+        [field: SerializeField] public AEffect[] Effects { get; private set; }
+    }
+}
