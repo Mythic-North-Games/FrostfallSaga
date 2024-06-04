0554a51f3753a8a40e0228e98dbb48c1b7a12b07
Alexandre Sparton <alexandre.sparton@epitech.eu>
2024-06-04 15:55:30 +0200

    feat(fighter): created fighter configuration scriptable object + prefab

---
+++ Assets/Code/Scripts/Fight/Fighter/FighterStats.cs
@@ -0,0 +1,18 @@
+namespace FrostfallSaga.Fight.Fighters
+{
+    public class FighterStats
+    {
+        public int health;
+        public int maxHealth;
+        public int actionPoints;
+        public int maxActionPoints;
+        public int movePoints;
+        public int maxMovePoints;
+        public int strength;
+        public int dexterity;
+        public int magicalStrength;
+        public int physicalResistance;
+        public int magicalResistance;
+        public int initiative;
+    }
+}
