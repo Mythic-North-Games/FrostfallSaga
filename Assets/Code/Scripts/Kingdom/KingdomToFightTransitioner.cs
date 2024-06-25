557d0de3c7873e3a6e59ee126d99ffe8aa7aa5ca
toto toto <toto@toto.fr>
2024-06-25 14:39:17 +0200

    feat: transition to fight map

--- Assets/Code/Scripts/Kingdom/KingdomToFightTransitioner.cs
+++ Assets/Code/Scripts/Kingdom/KingdomToFightTransitioner.cs
@@ -13,8 +13,10 @@ namespace FrostfallSaga.Kingdom
 		[SerializeField] private float _readyToFightAnimationDuration = 2f;
 		[SerializeField] private float _delayBeforeLoadingSceneAfterReadyAnimation = 2f;
 		[SerializeField] private string _fightSceneName;
+        [field: SerializeField] public KingdomConfigurationSO KingdomConfiguration { get; private set; }
 
-		public void OnEnable()
+
+        public void OnEnable()
 		{
 			_kingdomManager.OnEnemiesGroupEncountered += OnEnemiesGroupEncountered;
 		}
@@ -28,35 +30,52 @@ namespace FrostfallSaga.Kingdom
 		/// Plays a ready to fight animation then make the initiating group move to the targeted group then load fight scene.
 		/// </summary>
 		private IEnumerator PlayReadyAnimationThenLoadScene(EntitiesGroup heroGroup, EnemiesGroup enemiesGroup, bool heroGroupInitiating)
-		{
-			Entity heroEntity = heroGroup.GetDisplayedEntity();
-			Entity enemyEntity = enemiesGroup.GetDisplayedEntity();
+        {
+            Entity heroEntity = heroGroup.GetDisplayedEntity();
+            Entity enemyEntity = enemiesGroup.GetDisplayedEntity();
 
-			// Make groups rotate to watch each other
-			heroEntity.EntityVisualMovementController.RotateTowardsCell(enemiesGroup.Cell);
-			enemyEntity.EntityVisualMovementController.RotateTowardsCell(heroGroup.Cell);
+            // Make groups rotate to watch each other
+            heroEntity.EntityVisualMovementController.RotateTowardsCell(enemiesGroup.Cell);
+            enemyEntity.EntityVisualMovementController.RotateTowardsCell(heroGroup.Cell);
 
-			// Play ready to fight animation for a while
-			heroEntity.EntityAnimationController.PlayAnimationState("ReadyToFight");
-			enemyEntity.EntityAnimationController.PlayAnimationState("ReadyToFight");
-			yield return new WaitForSeconds(_readyToFightAnimationDuration);
+            // Play ready to fight animation for a while
+            heroEntity.EntityAnimationController.PlayAnimationState("ReadyToFight");
+            enemyEntity.EntityAnimationController.PlayAnimationState("ReadyToFight");
+            yield return new WaitForSeconds(_readyToFightAnimationDuration);
 
-			// Make initiator group go to the cell of its enemy
-			if (heroGroupInitiating)
-			{
-				heroGroup.MoveToCell(enemiesGroup.Cell, true);
-			}
-			else
-			{
-				enemiesGroup.MoveToCell(heroGroup.Cell, true);
-			}
-			yield return new WaitForSeconds(_delayBeforeLoadingSceneAfterReadyAnimation);
+            // Make initiator group go to the cell of its enemy
+            if (heroGroupInitiating)
+            {
+                heroGroup.MoveToCell(enemiesGroup.Cell, true);
+            }
+            else
+            {
+                enemiesGroup.MoveToCell(heroGroup.Cell, true);
+            }
 
-			// Start the fight scene
-			_sceneTransitioner.FadeInToScene(_fightSceneName);
-		}
+            PersisteKingdomConfiguration();
+            yield return new WaitForSeconds(_delayBeforeLoadingSceneAfterReadyAnimation);
 
-		private void OnDisable()
+            // Start the fight scene
+            _sceneTransitioner.FadeInToScene(_fightSceneName);
+        }
+
+        private void PersisteKingdomConfiguration()
+        {
+            if (KingdomConfiguration == null)
+            {
+                Debug.LogError("KingdomCOnfigurationSO is null");   
+            } else
+            {
+                KingdomConfiguration.HexGrid = _kingdomManager.KingdomGrid;
+                KingdomConfiguration.HeroGroup = _kingdomManager.HeroGroup;
+                KingdomConfiguration.EnemiesGroups = _kingdomManager.EnemiesGroups;
+                JsonUtility.ToJson(KingdomConfiguration);
+                Debug.Log("KingdomConfiguration Saved !");
+            }
+        }
+
+        private void OnDisable()
 		{
 			_kingdomManager.OnEnemiesGroupEncountered -= OnEnemiesGroupEncountered;
 		}
