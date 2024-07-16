1b4079d9813f3a012ae476db869e3012ec4e1588
cedricRLB <105118045+cedricRLB@users.noreply.github.com>
2024-07-16 14:32:40 +0200

    feat: ajout des sources de l'ui du paneau de deboggage

---
+++ Assets/DebugPannel/DebugPannelScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace FrostfallSaga
+{
+    public class DebugPannelScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var objCmdList1 = GetComponent<Button>("CmdList1")
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
