ebd03d2bd7287ced465f92484c850784fb4bbec8
Alexis <alexis.guyon@epitech.eu>
2024-09-26 16:08:09 +0200

    FEAT: adding Tree and modified Evaluate method bcs he was protected and not public (mb ^^)

---
+++ Assets/Code/Scripts/Fight/Controllers/AI/Tree.cs
@@ -0,0 +1,30 @@
+﻿using Codice.Client.Common.TreeGrouper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FrostfallSaga.Fight.Assets.Code.Scripts.Fight.Controllers.AI
+{
+    public abstract class Tree : MonoBehaviour
+    {
+        private Node _node = null;
+
+        protected void Start()
+        {
+            _node = SetupTree();
+        }
+
+        private void Update()
+        {
+            if (_node != null)
+            {
+                _node.Evaluate();
+            }
+        }
+
+        protected abstract Node SetupTree();
+    }
+}
