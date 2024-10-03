918a38d18eb03e9b53a1360ce2663ecb46ca9327
Alexis <alexis.guyon@epitech.eu>
2024-10-03 17:10:41 +0200

    feat: ffs-213 Create selector.cs

---
+++ Assets/Code/Scripts/Fight/Controllers/AI/Selector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrostfallSaga.Fight.Assets.Code.Scripts.Fight.Controllers.AI
+{
+    public class Selector : Node
+    {
+        public Selector() : base { }
+        public Selector(List<Node> children) : base(children) { }
+
+        public override NodeState Evaluate()
+        {
+            foreach (Node node in children)
+            {
+                switch (node.Evaluate)
+                {
+                    case NodeState.FAILURE:
+                        continue;
+                    case NodeState.SUCCESS:
+                        return NodeState.SUCCESS;
+                    case NodeState.RUNNING:
+                        return NodeState.RUNNING;
+                    default:
+                        continue;
+                }
+            }
+            return NodeState.FAILURE;
+        }
+    }
