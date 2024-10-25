637e44dc3e7bf608c94a0adc29e17928d10d5619
Alexis <alexis.guyon@epitech.eu>
2024-10-25 16:13:33 +0200

    feat: wip action

---
+++ Assets/Code/Scripts/Fight/Controllers/BehaviourTreeController/Actions/DamageTargetAction.cs
@@ -0,0 +1,13 @@
+﻿using FrostfallSaga.Fight.Fighters;
+using FrostfallSaga.Grid;
+
+namespace FrostfallSaga.Fight.Controllers.BehaviourTreeController.Actions
+{
+    public class DamageTargetAction : FBTNode
+    {
+        public DamageTargetAction(Fighter possessedFighter, HexGrid fightGrid) : base(Fighter possessedFighter, HexGrid fightGrid)
+        { 
+
+        }
+    }
+}
