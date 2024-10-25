2237205e1efebf0be224e801518a03ce9d2e0d4d
Alexandre Sparton <alexandre.sparton@epitech.eu>
2024-10-25 15:30:17 +0200

    feat(ApplyStatusEffectSO): moved status from abilitySO to a dedicated effect

---
+++ Assets/Code/Scripts/Fight/Effects/ApplyStatusEffectSO.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using FrostfallSaga.Fight.Fighters;
+using FrostfallSaga.Fight.Statuses;
+
+namespace FrostfallSaga.Fight.Effects
+{
+    /// <summary>
+    /// Effect that applies the configured status to the target fighter.
+    /// </summary>
+    [CreateAssetMenu(fileName = "RemoveStatusEffect", menuName = "ScriptableObjects/Fight/Effects/RemoveStatusEffect", order = 0)]
+    public class ApplyStatusesEffectSO : AEffectSO
+    {
+        [field: SerializeField, Tooltip("The status to apply.")] public AStatus[] StatusesToApply { get; private set; } = { };
+
+        public override void ApplyEffect(Fighter initiator, Fighter receiver, bool canMasterstroke = false, bool canDodge = false)
+        {
+            foreach (AStatus status in StatusesToApply)
+            {
+                receiver.ApplyStatus(status);
+                Debug.Log($"Status {status.Name} applied to {receiver.name}.");
+            }
+        }
+    }
+}
