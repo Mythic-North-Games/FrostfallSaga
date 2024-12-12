8e08a2a1cf4b0f9bc22123c74098f5102efc459a
Xerataw <20210752+Xerataw@users.noreply.github.com>
2024-12-12 13:40:21 +0100

    docs: better param documentation

--- Assets/Code/Scripts/Audio/AudioManager.cs
+++ Assets/Code/Scripts/Audio/AudioManager.cs
@@ -23,13 +23,16 @@ namespace FrostfallSaga.Audio
 
         /// <summary>
         /// Create an audioSource gameObject in the scene and play the audioClip, then delete the gameObject
+        /// <paramref name="audioClip"/> The audio clip to play
+        /// <paramref name="spawnTransform"/> The transform to spawn the audioSource gameObject
+        /// <paramref name="audioVolume"/> The volume of the audio clip
         /// </summary>
-        public void PlaySoundEffectClip(AudioClip audioClip, Transform spawnTransform, float volume)
+        public void PlaySoundEffectClip(AudioClip audioClip, Transform spawnTransform, float audioVolume)
         {
             float clipLength = audioClip.length;
             AudioSource audioSource = Instantiate(audioSourceObject, spawnTransform.position, Quaternion.identity);
             audioSource.clip = audioClip;
-            audioSource.volume = volume;
+            audioSource.volume = audioVolume;
             audioSource.Play();
             Destroy(audioSource.gameObject, clipLength);
         }
