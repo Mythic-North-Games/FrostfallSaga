60740368533ef58623c4e1b12f3163264377c25f
addevep <antoine.duquenne@epitech.eu>
2025-04-25 16:05:58 +0200

    feat: new highlight && fix visualCells without ignore raycast

---
+++ Assets/Code/Scripts/Utils/GameObjectVisuals/HighlightColorData.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace FrostfallSaga.Utils
+{
+    [Serializable]
+    public class HighlightColorData
+    {
+        public HighlightColor colorType;
+        public Color color;
+    }
+}
